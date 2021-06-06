9a2dddb533207dcc446d7348f6057fe302972515
Reza <r871017h800501@gmail.com>
2021-06-07 00:20:16 +0430

    error handling, DEBUG=False on host, fix persian title in url & image loading-r.

---
+++ mysqlclient problem in host/__init__.py
@@ -0,0 +1,5 @@
+# pip install pymysql
+# put on bottom codes in root_project.__init__.py (the same directory of setting.py)
+import pymysql
+
+pymysql.install_as_MySQLdb()
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
+import itertools
+
 from django.utils.text import slugify
 import random
 import string
 from datetime import datetime
+
 
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
